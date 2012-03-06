9826c7f2fdb17732871094bcd2561f7721bc9110
Scott Burton <scottburton11@gmail.com>
2012-03-06 10:43:25 -0800

    cache responses for 24 hours

--- app/controllers/postal_codes_controller.rb
+++ app/controllers/postal_codes_controller.rb
@@ -1,6 +1,7 @@
 class PostalCodesController < ApplicationController
   respond_to :json
   def show
+    response.headers['Cache-Control'] = 'public, max-age=86400'
     @code = PostalCode.search(params[:id])
     respond_with @code
   end
